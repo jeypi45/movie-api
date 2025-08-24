5a1c66ec73749830808c509d504a8fcf83b00d40
JohnPaul <leidoj7@gmail.com>
2025-08-24 09:34:01 +0800

    Implement CRUD operations for movie management

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -11,7 +11,10 @@ exports.create = (req, res) => {
         title: req.body.title,
         director: req.body.director,
         release_year: req.body.release_year,
-        genre: req.body.genre
+        genre: req.body.genre,
+        rating: req.body.rating,
+        duration: req.body.duration,
+        language: req.body.language
     };
 
     db.query("INSERT INTO movies SET ?", movie, (err, data) => {
@@ -52,6 +55,63 @@ exports.findOne = (req, res) => {
         } else {
             res.status(404).send({
                 message: `Movie not found with id ${req.params.id}`
+            });
+        }
+    });
+};
+
+exports.update = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Content cannot be empty!"
+        });
+    }
+
+    const id = req.params.id;
+    const movie = {
+        title: req.body.title,
+        director: req.body.director,
+        release_year: req.body.release_year,
+        genre: req.body.genre,
+        rating: req.body.rating,
+        duration: req.body.duration,
+        language: req.body.language
+    };
+
+    db.query("UPDATE movies SET ? WHERE id = ?", [movie, id], (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Error updating movie with id " + id
+            });
+        } else if (data.affectedRows == 0) {
+            res.status(404).send({
+                message: `Movie not found with id ${id}`
+            });
+        } else {
+            res.send({
+                message: "Movie was updated successfully.",
+                id: id,
+                ...movie
+            });
+        }
+    });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    db.query("DELETE FROM movies WHERE id = ?", id, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Error deleting movie with id " + id
+            });
+        } else if (data.affectedRows == 0) {
+            res.status(404).send({
+                message: `Movie not found with id ${id}`
+            });
+        } else {
+            res.send({
+                message: "Movie was deleted successfully!"
             });
         }
     });
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -5,5 +5,6 @@ const movies = require('../controllers/movie.controller.js');
 router.post('/', movies.create);
 router.get('/', movies.findAll);
 router.get('/:id', movies.findOne);
-
+router.put('/:id', movies.update);
+router.delete('/:id', movies.delete);
 module.exports = router;
